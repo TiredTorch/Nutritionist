34eef0e9215eb93f1d6c4c1c0b50cc08ff037ef6
Tr.Torch <vitochek337@gmail.com>
2021-05-18 18:37:51 +0300

    Add 2 card-box and slider with slick

--- js/script.js
+++ js/script.js
@@ -2,4 +2,34 @@ $(document).ready(function() {
     $('.header__button').click(function() {
         $('.header__button, .header__menu').toggleClass('active');
     })
+    $('.header__link').click(function() {
+        $('.header__button, .header__menu').removeClass('active');
+    })
+
+    $('.slider__main').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 2
+                }
+              },
+
+          {
+            breakpoint: 900,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 })
